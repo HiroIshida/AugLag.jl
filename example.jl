33001f018f57340f539e5c8384619773a8aa0b06
HiroIshida <spitfire.docomo@gmail.com>
2021-01-30 15:18:44 +0900

    comparison with NLOPT slsqp

--- example.jl
+++ example.jl
@@ -2,9 +2,9 @@ using Revise
 using AugLag
 using LinearAlgebra
 using BenchmarkTools
+using NLopt
 
 AugLag.debugging() = true
-qm = QuadraticModel(0.0, [0, 0], Diagonal([1, 1.]))
 
 function eq_const(x::Vector{Float64})
     val = (x[1] - 1.0)^2 - x[2]
@@ -20,11 +20,49 @@ function ineq_const(x::Vector{Float64})
     #return Vector{Float64}(undef, 0), Matrix{Float64}(undef, 2, 0)
 end
 
+function nlopt_main()
+    qm = QuadraticModel(0.0, [0, 0], Diagonal([1, 1.]))
+
+    function objective(x::Vector, grad::Vector)
+        val, grad_ = qm(x)
+        if length(grad) > 0
+            grad[:] = grad_
+        end
+        return val
+    end
+
+    function eq_const(x::Vector, grad::Vector)
+        val = (x[1] - 1.0)^2 - x[2]
+        if length(grad) > 0
+            grad[1] = - 2 * (x[1] - 1.0)
+            grad[2] = 1.0
+        end
+        return - val
+    end
+
+    function ineq_const(x::Vector, grad::Vector)
+        val = (x[1] - 1.0) - x[2]
+        if length(grad) > 0
+            grad[1] = -1.0
+            grad[2] = 1.0
+        end
+        return -val
+    end
+
+    opt = Opt(:LD_SLSQP, 2)
+    opt.min_objective = objective
+    opt.inequality_constraint = ineq_const
+    opt.equality_constraint = eq_const
+    opt.xtol_abs = 1e-6
+    minf, minx, ret = optimize(opt, [2.0, 2.0])
+end
+
 function main()
+    qm = QuadraticModel(0.0, [0, 0], Diagonal([1, 1.]))
     prob = Problem(qm, eq_const, ineq_const, 2)
     x_opt = [2.0, 2.0]
     internal_data = gen_init_data(prob)
-    xtol = 1e-4
+    xtol = 1e-6
     for i in 1:20
         x_opt_pre = x_opt
         x_opt = step_auglag(x_opt, prob, internal_data, xtol)
@@ -33,4 +71,7 @@ function main()
         end
     end
 end
-@btime main()
+println("testing auglag")
+@benchmark main()
+println("testing nlopt slsqp")
+@benchmark nlopt_main()
