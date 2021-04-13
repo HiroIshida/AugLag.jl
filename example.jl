3e38ba63564393618ec0a915890355faa499b51d
HiroIshida <spitfire.docomo@gmail.com>
2021-04-13 16:28:24 +0900

    example

--- example.jl
+++ example.jl
@@ -10,14 +10,12 @@ function eq_const(x::Vector{Float64})
     val = (x[1] - 1.0)^2 - x[2]
     grad = transpose([2 * (x[1] - 1.0) -1.0;])
     return [val], grad
-    #return Vector{Float64}(undef, 0), Matrix{Float64}(undef, 2, 0)
 end
 
 function ineq_const(x::Vector{Float64})
     val = x[1] - 2
     grad = transpose([1 0;])
     return [val], grad
-    #return Vector{Float64}(undef, 0), Matrix{Float64}(undef, 2, 0)
 end
 
 function debug_plot(ws::Workspace, b_min, b_max, f, g, h, x, Lgrad, newton_direction)
@@ -40,11 +38,11 @@ function main()
     x = [2.0, 2.0]
     ws = Workspace(2, 1, 1)
     cfg = Config()
-    for i in 1:5
-        println(i)
+    for i in 1:100
+        x_pre = copy(x)
         x = single_step!(ws, x, qm, ineq_const, eq_const, cfg)
-        println(x)
         shoud_abort(ws, cfg) && break
+        norm(x_pre - x) < 1e-5 && break
     end
     @test isapprox(x, [2.0, 1.0], atol=1e-3)
 end
